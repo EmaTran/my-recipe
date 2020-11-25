73fe451fd4d4bb05bf1b9dcb68b651bb7a4aa686
suzu_SSH_Key <avril8319@gmail.com>
2020-11-24 22:38:13 -0800

    change

---
+++ src/main/java/springmvc/User.java
@@ -0,0 +1,67 @@
+package springmvc;
+
+import java.util.Objects;
+
+public class User {
+
+    private int id;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User user = (User) obj;
+        return id == user.id;
+    //    return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+    //    return super.hashCode();
+        return Objects.hash(id);
+    }
+}
